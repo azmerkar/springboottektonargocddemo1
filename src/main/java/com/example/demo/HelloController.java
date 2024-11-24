deebe5423eeb7c237a88f56dec6ccd86eb1e7845
mehmet <mkaradayi4343@gmail.com>
2024-11-24 23:47:57 +0100

    changed hello.html

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -11,6 +11,23 @@
 //            return "Hello, Openshift" ;
 //      }
 // }
+
+// --------------------
+// package com.example.demo;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class HelloController {
+
+//     @GetMapping("/hello")
+//     public String sayHello() {
+//         // templates/hello.html dosyasını döndürüyor
+//         return "hello";
+//     }
+// }
+
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
@@ -21,7 +38,8 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        // templates/hello.html dosyasını döndürüyor
-        return "hello";
+        // static/hello.html dosyasına yönlendiriyor
+        return "redirect:/hello.html";
     }
 }
+
