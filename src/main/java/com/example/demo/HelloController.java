da9ad8c24fe769596e369bffebf13c1f022985c8
mehmet <mkaradayi4343@gmail.com>
2024-11-24 23:30:57 +0100

    changed hello.html

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,27 @@
-package com.example.demo; 
+// package com.example.demo; 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController; 
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController; 
 
-@RestController
-public class HelloController {
+// @RestController
+// public class HelloController {
     
-     @GetMapping("/hello")
-     public String sayHello() {
-           return "Hello, Openshift" ;
-     }
+//      @GetMapping("/hello")
+//      public String sayHello() {
+//            return "Hello, Openshift" ;
+//      }
+// }
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        // templates/hello.html dosyasını döndürüyor
+        return "hello";
+    }
 }
